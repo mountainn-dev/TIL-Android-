8482cec8c9228678c61e3b963167b5ff60c2123b
sungsaaan <32791382+sungsaaan@users.noreply.github.com>
2022-09-17 20:02:20 +0900

    22.09.17 자바의 정석 scanner, 가위바위보 게임, 반복문 학습

---
+++ SelfJava/src/Main.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+// 계절 구하기
+//        System.out.println("월을 입력하시오: ");
+//
+//        Scanner scanner = new Scanner(System.in);
+//        int month = scanner.nextInt();
+//        switch (month) {
+//            case 3: case 4: case 5:
+//                System.out.println("현재 계절은 봄");
+//                break;
+//            case 6: case 7: case 8:
+//                System.out.println("현재 계절은 여름");
+//                break;
+//            default:
+//                System.out.println("모르겠다");
+//        }
+
+// 가위바위보 게임
+//        System.out.println("1->가위, 2->바위, 3->보 중 하나를 입력하세요: ");
+//        Scanner scanner = new Scanner(System.in);
+//        int userNum = scanner.nextInt();
+//        if (userNum == 1) System.out.println("가위를 선택함");
+//        else if (userNum == 2) System.out.println("바위를 선택함");
+//        else if (userNum == 3) System.out.println("보를 선택함");
+//        int comNum = (int)(Math.random() * 3) + 1;   // 0.0 <= Math.random() < 1.0 double형 난수값을 이용해 1~3 난수 생성
+//        if (comNum == 1) System.out.println("컴퓨터는 가위를 선택함");
+//        else if (comNum == 2) System.out.println("컴퓨터는 바위를 선택함");
+//        else if (comNum == 3) System.out.println("컴퓨터는 보를 선택함");
+//        switch (userNum-comNum) {
+//            case -2: case 1:
+//                System.out.printf("당신 이김");
+//                break;
+//            case -1: case 2:
+//                System.out.println("당신 짐");
+//                break;
+//            default:
+//                System.out.println("비김");
+//        }
+
+
+// 학점 계산기
+//        System.out.println("점수를 입력하세요: ");
+//        Scanner scanner = new Scanner(System.in);
+//        int score = scanner.nextInt();
+//
+//        switch (score/10) {
+//            case 10: case 9:
+//                System.out.println("학점 A");
+//                break;
+//            default:
+//                System.out.println("학점 F");
+//        }
+
+// for문
+//        int sum = 0;
+//        for (int i = 1; i <= 10; i++) {
+//            sum += i;
+//            System.out.printf("1부터 %d까지의 합: %d\n", i, sum);
+//        }
+
+//        String star = "*";
+//        for (int i = 0; i <= 5; i++) {
+//            for (int j = 0; j <= 5; j++) System.out.printf(star);
+//            System.out.println();
+//        }
+//
+//        for (int i = 1; i <= 5; i++) {
+//            for (int j = 0; j < i; j++) System.out.printf(star);
+//            System.out.println();
+//        }
+
+//        for (int i = 0; i <= 5; i++) {
+//            System.out.printf(star.repeat(i));
+//            System.out.println();
+//        }
+    }
+}
