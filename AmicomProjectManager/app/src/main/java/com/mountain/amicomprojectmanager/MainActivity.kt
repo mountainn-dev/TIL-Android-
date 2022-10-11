3cef82dbfc692188622f2b52159992e738fd8526
sungsaaan <32791382+sungsaaan@users.noreply.github.com>
2022-10-11 22:44:31 +0900

    22.10.11 메인 액티비티 생성 시 DB의 프로젝트 키 리스트 생성 기능 구현

--- AmicomProjectManager/app/src/main/java/com/mountain/amicomprojectmanager/MainActivity.kt
+++ AmicomProjectManager/app/src/main/java/com/mountain/amicomprojectmanager/MainActivity.kt
@@ -24,18 +24,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val myListener = object : ValueEventListener {
-//            override fun onDataChange(snapshot: DataSnapshot) {
-//                val b = snapshot.child("ProjectList").children
-//                for (i in b){
-//                    Log.i("info", "${i.key}")
-//                }
-//            }
-//            override fun onCancelled(error: DatabaseError) {
-//                TODO("Not yet implemented")
-//            }
-//        }
-//        database.addValueEventListener(myListener)
+        // TODO: db 에서 플젝 키리스트 불러오고 해당 리스트 인덱싱해서 value 가져오는 코드 구현
+        val myListener = object : ValueEventListener {
+            // TODO: 왜인지는 모르겠으나, onDataChange 로 액티비티 생성 단계에서 초기 데이터를 불러올 수가 있다.
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val c = snapshot.child("ProjectKeyList").children
+                for (i in c) projectKeyList.add(i.value.toString())
+//                val b = snapshot.child("ProjectList").child("projectName")
+//                Log.i("info", "$b")
+            }
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        }
+        database.addValueEventListener(myListener)
         if (projectList.size == 0) binding.btnAddProjectGuider.isVisible = true
         // 재적용은 최적화에 좋지 않기 때문에 onCreate 에서 어댑터 적용을 한 번만 해준다.
         binding.lvProject.adapter = mAdapter
@@ -75,10 +77,11 @@ class MainActivity : AppCompatActivity() {
                         ) {
                             projectKeyList.clear()
 
+                            // 가장 최근에 추가된 key value 하나만을 접근할 수가 없어서 리스트를 초기화하고 그 때
+                            // 그 때 다시 키 리스트 전체를 갱신하는 식으로 구현했다.
                             val a = snapshot.children
                             for (snapshot in a) projectKeyList.add(snapshot.key.toString())
                             database.child("ProjectKeyList").setValue(projectKeyList)
-
                         }
 
                         override fun onChildChanged(
@@ -105,6 +108,8 @@ class MainActivity : AppCompatActivity() {
 
                     }
                     // TODO: child가 아닌 database의 Listener로 설정했더니 됐음! -> 오버헤드 가능성이 있다.
+                    // child 리스너의 경우, database의 리스너로 설정해줘야 child add 카운트가 1번으로 작동하였고
+                    // ProjectList 리스너로 설정할 경우, child add 카운트가 고정되지 않고 계속해서 증가했다.
                     database.addChildEventListener(dbListener)
                 }
                 if (projectList.size != 0) binding.btnAddProjectGuider.isVisible = false
